5e1061eca29331a1f4555981b84d36cca70b6e01
Michał Barczak <michalbarczak@o2.pl>
2020-07-03 14:16:50 +0200

    Front in VUE

---
+++ Modul2/orders_client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Client msg="Orders"/>
+  </div>
+</template>
+
+<script>
+import Client from './components/Client.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
